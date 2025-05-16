4a36f25ce6fdcf524bb3bc65d10fc9c6a4a86af9
Light360hub <ujjawalkumar21202@gmail.com>
2025-05-16 23:19:21 +0530

    Initial commit

---
+++ src/main/java/com/DoctorSuggestion/DoctorSuggestion/Doctor_SuggestionApplication.java
@@ -0,0 +1,13 @@
+package com.DocSuggest.DocSuggest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Doctor_SuggestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Doctor_SuggestionApplication.class, args);
+	}
+
+}
